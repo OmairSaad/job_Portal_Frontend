b8d761557a295ff8f97c04e535713f0428f969cc
OmairSaad <alamomair724@gmail.com>
2025-01-12 16:39:38 +0530

    HistoryCard component is added

--- src/JobHistory/JobHistory.tsx
+++ src/JobHistory/JobHistory.tsx
@@ -1,7 +1,68 @@
-const JobHistory = ()=>{
+import { Tabs, TabsList } from "@mantine/core";
+import HistoryCard from "./HistoryCard";
+import JobData from "../Data/JobData";
+
+const JobHistory = () => {
     return (
         <div>
-            Omaira;lam
+            <div className="text-2xl font-semibold mb-5 pt-5">Job History</div>
+
+            <Tabs variant="outline" radius="lg" defaultValue="applied">
+                <TabsList className=" [&_button]:font-semibold [&_button]:!text-lg mb-5 [&_button[data-active='true']]:text-bright-sun-400">
+                    <Tabs.Tab value="applied">Applied</Tabs.Tab>
+                    <Tabs.Tab value="saved">Saved</Tabs.Tab>
+                    <Tabs.Tab value="offered">Offered</Tabs.Tab>
+                    <Tabs.Tab value="interviewing">Interviewing</Tabs.Tab>
+                </TabsList>
+
+                <Tabs.Panel value="applied">
+                    <div className="flex flex-wrap gap-5 mt-5">
+                        {
+                            JobData.map((job) => {
+                                return (
+                                    <HistoryCard jobdetails={job} applied={true} saved={false} offered={false} interviewing={false}/>
+                                )
+                            })
+                        }
+                    </div>
+                </Tabs.Panel>
+
+                <Tabs.Panel value="saved">
+                    <div className="flex flex-wrap gap-5 mt-10">
+                        {
+                            JobData.map((job) => {
+                                return (
+                                    <HistoryCard jobdetails={job} applied={false} saved={true} offered={false} interviewing={false} />
+                                )
+                            })
+                        }
+                    </div>                
+                </Tabs.Panel>
+
+                <Tabs.Panel value="offered">
+                    <div className="flex flex-wrap gap-5 mt-10">
+                        {
+                            JobData.map((job) => {
+                                return (
+                                    <HistoryCard jobdetails={job} applied={false} saved={false} offered={true} interviewing={false} />
+                                )
+                            })
+                        }
+                    </div>                
+                </Tabs.Panel>
+
+                <Tabs.Panel value="interviewing">
+                    <div className="flex flex-wrap gap-5 mt-10">
+                        {
+                            JobData.map((job) => {
+                                return (
+                                    <HistoryCard jobdetails={job} applied={false} saved={false} offered={false} interviewing={true}/>
+                                )
+                            })
+                        }
+                    </div>
+                </Tabs.Panel>
+            </Tabs>
         </div>
     )
 }
